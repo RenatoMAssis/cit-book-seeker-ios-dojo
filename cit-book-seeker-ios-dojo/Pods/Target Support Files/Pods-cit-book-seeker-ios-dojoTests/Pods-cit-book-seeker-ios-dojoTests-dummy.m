d15172706c87d7c7ec699aaec37915156648018a
zied <zied-akoubi@hotmail.com>
2019-12-12 13:57:50 -0300

    add pod files

---
+++ cit-book-seeker-ios-dojo/Pods/Target Support Files/Pods-cit-book-seeker-ios-dojoTests/Pods-cit-book-seeker-ios-dojoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cit_book_seeker_ios_dojoTests : NSObject
+@end
+@implementation PodsDummy_Pods_cit_book_seeker_ios_dojoTests
+@end
---
+++ cit-book-seeker-ios-dojo/Pods/Target Support Files/Pods-cit-book-seeker-ios-dojoUITests/Pods-cit-book-seeker-ios-dojoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cit_book_seeker_ios_dojoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_cit_book_seeker_ios_dojoUITests
+@end
